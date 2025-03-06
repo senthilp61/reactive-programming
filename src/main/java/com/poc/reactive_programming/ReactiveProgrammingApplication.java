c7445a072700348bdca8c3739c7cc4834423102b
SENTHIL PRAKASH P <senthil_prakashp@apple.com>
2025-03-06 18:29:58 +0530

    First Reactive Program

--- src/main/java/com/poc/reactive_programming/ReactiveProgrammingApplication.java
+++ src/main/java/com/poc/reactive_programming/ReactiveProgrammingApplication.java
@@ -2,6 +2,8 @@ package com.poc.reactive_programming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class ReactiveProgrammingApplication {
@@ -9,6 +11,18 @@ public class ReactiveProgrammingApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(ReactiveProgrammingApplication.class, args);
+
+		Flux<String> fruitFlux = Flux.just("Apple", "Banana", "Mango", "Orange");
+
+		fruitFlux
+				.map(String::toUpperCase)
+				.subscribe(fruit -> System.out.println("Fruit : "+fruit));
+
+		Mono<String> singleFruit = Mono.just("Strawberry");
+
+		singleFruit
+				.map(String::toUpperCase)
+				.subscribe(fruit -> System.out.println("Single Fruit: "+fruit));
 	}
 
 }
